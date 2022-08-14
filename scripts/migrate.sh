37319c0a343cd4984a01ed00509ac3612468a936
Touraj <2rajpx@gmail.com>
2022-08-15 02:44:29 +0400

    chore(scripts): add reset option to migrate

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -4,7 +4,25 @@ description() {
     echo "Makes migration files and executes their commands";
 }
 
+help() {
+    echo "Available Options:"
+
+    echo "  -r"
+    echo "  Removes database, creates it again and then executes migrations commands"
+    echo
+}
+
 run() {
-    $APP sf doctrine:migrations:diff
-    $APP sf doctrine:migrations:migrate
+    if [[ " ${@:1} " =~ " -r " ]]; then
+        $APP sf doctrine:database:drop --if-exists --force --no-interaction
+        $APP sf doctrine:database:create --if-not-exists --no-interaction
+    fi
+    $APP sf doctrine:migrations:diff \
+        --no-interaction \
+        --formatted \
+        --allow-empty-diff
+    $APP sf doctrine:migrations:migrate \
+        --allow-no-migration \
+        --all-or-nothing \
+        --no-interaction
 }
