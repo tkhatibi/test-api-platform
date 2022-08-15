1abeacdf902959de782c107084dd07adc87906cf
Touraj <2rajpx@gmail.com>
2022-08-15 04:44:33 +0400

    feat: add lexik jwt

--- scripts/init.sh
+++ scripts/init.sh
@@ -23,6 +23,12 @@ run() {
 
     $APP dcu --build --renew-anon-volumes
 
+    # https://api-platform.com/docs/core/jwt/#installing-lexikjwtauthenticationbundle
+    $APP dcx php apk add openssl
+    $APP dcx php php bin/console lexik:jwt:generate-keypair --skip-if-exists --no-interaction
+    $APP dcx php setfacl -R -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
+    $APP dcx php setfacl -dR -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt
+
     # https://github.com/dunglas/symfony-docker/blob/main/docs/troubleshooting.md#fix-chromebrave-ssl
     if [ "$OS" = 'Linux' ]; then
         docker cp $($APP dc ps -q caddy):/data/caddy/pki/authorities/local/root.crt /usr/local/share/ca-certificates/root.crt && sudo update-ca-certificates
